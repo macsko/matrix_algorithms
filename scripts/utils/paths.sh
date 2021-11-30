eda191faf5d4c4a4baf8efabf8c8638484733d21
Kacper Kafara <kacperkafara@gmail.com>
2021-12-01 00:44:20 +0100

    Add cpp script for matrix generation to generate-matrix script

---
+++ scripts/utils/paths.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# these paths are relative to `generate-matrix.sh`
+
+__default_data_dir__="../output"
+
+__octave_generator__="octave/massmatrix-wrapper.m"
+
+__cpp_dir__="cpp"
+
+__cpp_generator__="${__cpp_dir__}/out/gen"
+
+__cpp_generator_buildscript_name__="buildscript.sh"
+
+__resolve_matrix_size__="utils/resolve-matrix-size.py"
