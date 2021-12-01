fcfe5e480928fd97dfb48abda100da91cdaa9df3
Kacper Kafara <kacperkafara@gmail.com>
2021-12-01 01:28:27 +0100

    Change data type in cpp script from double to long double and
    filter matrix values that are <= 1e-9

--- scripts/cpp/src/Matrix.hpp
+++ scripts/cpp/src/Matrix.hpp
@@ -3,6 +3,14 @@
 
 #include <iostream>
 
+inline long double abs_(long double value) noexcept {
+	return (value >= 0 ? value : -value);
+}
+
+inline bool epseq(long double value1, long double value2, long double eps) noexcept {
+	return abs_(value1 - value2) <= eps;
+}
+
 struct Index{
     int r;
     int c;
@@ -14,12 +22,12 @@ struct Index{
 };
 
 struct Matrix{
-    double* values;
+    long double* values;
     int r;
     int c;
 
     Matrix(int r, int c){
-        this->values = new double[r*c];
+        this->values = new long double[r*c];
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 values[i*c + j] = 0;
@@ -30,13 +38,13 @@ struct Matrix{
         this->c = c;
     }
 
-    Matrix(double* values, int r, int c){
+    Matrix(long double* values, int r, int c){
         this->values = values;
         this->r = r;
         this->c = c;
     }
 
-    double& operator[](Index index){
+    long double& operator[](Index index){
         return values[index.r*c + index.c];
     }
 };
@@ -44,7 +52,7 @@ struct Matrix{
 std::ostream& operator<<(std::ostream& os, Matrix m){
 	for(int i=0; i<m.r; i++){
 		for(int j=0; j<m.c; j++){
-			if(m.values[i*m.c + j] != 0)
+			if(!epseq(m.values[i*m.c + j], 0, 1e-9))
 				os << '(' << j + 1  << ", " << i + 1 << ") ->  " << m.values[i * m.c + j] << "\n";
 		}
 	}
